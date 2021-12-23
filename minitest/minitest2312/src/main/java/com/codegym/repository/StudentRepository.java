42db783566a67c7f5c8945af926a4b05d07618ed
hoangducviet1993 <ducviet12vl@gmail.com>
2021-12-23 11:05:57 +0700

    done cơ ban

--- minitest/minitest2312/src/main/java/com/codegym/repository/StudentRepository.java
+++ minitest/minitest2312/src/main/java/com/codegym/repository/StudentRepository.java
@@ -4,20 +4,13 @@ import com.codegym.model.Student;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.Query;
-=======
->>>>>>> origin/master
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-<<<<<<< HEAD
     Page<Student> findByNameContaining(String firstName, Pageable pageable);
 
     @Query("select s from Student  s where s.score > 8 ")
     Page<Student>findNameByOrderScoreGreaterThan8(Pageable pageable);
-=======
-    Page<Student> findByNameContaining(String firstName , Pageable pageable);
->>>>>>> origin/master
 }
